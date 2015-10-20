8f3b6808d75f17430d0c67395441f9c97d06e3bf
jmc0592 <jmcole17@yahoo.com>
2015-10-20 09:46:36 -0500

    Added Scrapy library to repo, added some info to manual about UNT site, modified UNT scraper

--- site/python/untextbookfinder/untextbookfinder/spiders/untbookstore.py
+++ site/python/untextbookfinder/untextbookfinder/spiders/untbookstore.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+# Author: Jacob Cole
+# Class for scraping all books from the UNT bookstore
 import scrapy
-
 
 class UntbookstoreSpider(scrapy.Spider):
     name = "untbookstore"
@@ -10,13 +11,21 @@ class UntbookstoreSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        print response.xpath('//li/div/div/ul/li/text()').extract()
+        print response.xpath('//li[@class="deptColumn"]/ul').extract()
+        '''
         return [scrapy.FormRequest.from_response(
         	response,
             formdata={'data-optionvalue': '66350155', 'age': '27'},
             callback=self.after_post
         )]
+        '''
+        #possible post request url for department
+        #TextBookProcessDropdownsCmd?campusId=57996742&termId=66350155&deptId=66350669&courseId=&sectionId=&storeId=71237&catalogId=10001&langId=-1&dropdown=dept
 
+        #possible post request for course
+        #TextBookProcessDropdownsCmd?campusId=57996742&termId=66350155&deptId=66350399&courseId=66354964&sectionId=&storeId=71237&catalogId=10001&langId=-1&dropdown=course
     def after_post(self, response):
     	print "Got to after_post"
     	print response
+
+    #def getAllDepartmentNames():
