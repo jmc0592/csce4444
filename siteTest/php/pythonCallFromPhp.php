1c006adf7ca8dfe88f8ea5e933986a63945d53f7
jmc0592 <jmcole17@yahoo.com>
2015-10-24 21:25:39 -0500

    Changed python call in php to relative path (forgot to add file last time), added testDBConnection

--- siteTest/php/pythonCallFromPhp.php
+++ siteTest/php/pythonCallFromPhp.php
@@ -2,7 +2,7 @@
 	<body>
 		<h1>Page for testing python calls from php</h1>
 		<?php
-			$python = 'python /Applications/XAMPP/xamppfiles/htdocs/csce4444/siteTest/python/test.py';
+			$python = 'python ../python/test.py';
 			$result = [];
 			exec($python, $result);
 			echo var_export($result);
---
+++ siteTest/php/testDBConnect.php
@@ -0,0 +1,7 @@
+<?php
+
+	$link = mysqli_connect( "107.180.24.254", "scrubs", "CSc34444!", "csce4444");
+	if(!$link){
+		echo "Not connected";
+	}
+?>
