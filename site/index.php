9da88c09bf2c16980f535469d05bf79996cf5a18
parkercantu <parkercantu@yahoo.com>
2015-11-06 14:13:21 -0600

    Fixing removed files again + adding updates
    the formatting for the website is now updated to close to what it should
    be for release.

---
+++ site/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css\stylesheet.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>
+
+<body>
+	<div id = "banner">
+		<div id = "bannertext"><h1>UNTextbookFinder</h1></div>
+	</div>
+	<div id = "left">
+		<p><a href="">Home</a></p>
+		<p><a href="">About Us</a></p>
+	</div>
+	<div id = "right">
+		<div id = "select">
+			<select class = "selectmenus">
+				<option value="option1">option1</option>
+				<option value="option2">option2</option>
+				<option value="option3">option3</option>
+				<option value="option4">option4</option>
+			</select>
+			<select class = "selectmenus">
+				<option value="option1">option1</option>
+				<option value="option2">option2</option>
+				<option value="option3">option3</option>
+				<option value="option4">option4</option>
+			</select>
+			<select class = "selectmenus">
+				<option value="option1">option1</option>
+				<option value="option2">option2</option>
+				<option value="option3">option3</option>
+				<option value="option4">option4</option>
+			</select>
+			<a href="search/search.php">
+				<div id = "button">
+					<p>SEARCH &#9654;</p>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div id = "footer">
+	</div>
+</body>
--- site/search/search.php
+++ site/search/search.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
-<html>
 <head>
-	<title>Search</title>
+	<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
+
 <body>
-	<h1>Search Page</h1>
+	<div id = "banner">
+		<div id = "bannertext"><h1>UNTextbookFinder</h1></div>
+	</div>
+	<div id = "left">
+		<p><a href="index.php">Home</a></p>
+		<p><a href="">About Us</a></p>
+	</div>
+	<div id = "right">
+		<div id="bookinfo">
+			<div id="generalinfo">
+			</div>
+			<div id="voertmans" class="bookstore">
+			</div>
+			<div id="unt" class="bookstore">
+			</div>
+			<div id="chegg" class="bookstore">
+			</div>
+			<div id="amazon" class="bookstore">
+			</div>
+		</div>
+	</div>
+	<div id = "footer">
+	</div>
 </body>
-</html>
